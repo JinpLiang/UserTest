f30cbd71d71482052b7d496d6721437752174fb7
JinpLiang <1306774039@qq.com>
2019-10-04 09:44:51 +0800

    103

---
+++ MyMaven/src/main/java/com/hunau/HelloController.java
@@ -0,0 +1,15 @@
+package com.hunau;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/sayhello")
+    public String hello() {
+        return "梁景普 内蒙古乌兰察布市!";
+    }
+}
--- MyMaven/src/main/java/com/hunau/mymaven/MyMavenApplication.java
+++
@@ -1,4 +0,0 @@
-package java.com.hunau.mymaven;
-
-public class MyMavenApplication {
-}
